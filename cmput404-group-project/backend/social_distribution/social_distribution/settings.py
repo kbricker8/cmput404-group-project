13fe3478d20a93e89363a9be0c6d6ca463007ec8
Kyle <kbricker@ualberta.ca>
2023-03-24 00:38:56 -0600

    Update settings.py

--- cmput404-group-project/backend/social_distribution/social_distribution/settings.py
+++ cmput404-group-project/backend/social_distribution/social_distribution/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
