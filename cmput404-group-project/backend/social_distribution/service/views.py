a92bf5c15a52026fcd2b6fc6b4721840aee4ef79
Kyle <kbricker@ualberta.ca>
2023-03-25 16:45:51 -0600

    Fixed another bug

--- cmput404-group-project/backend/social_distribution/service/views.py
+++ cmput404-group-project/backend/social_distribution/service/views.py
@@ -50,7 +50,7 @@ class UsersViewSet(viewsets.GenericViewSet):
         user = User.objects.get(username=serializer.data.get("username"))
 
         author_uuid = uuid.uuid4()
-        id = baseURL+'/service/authors/' + str(author_uuid)
+        id = baseURL+'service/authors/' + str(author_uuid)
         author = Author(id=id, uuid=author_uuid, host=baseURL, displayName=serializer.data.get("username"), user=user)
         author.save()
         author.url = baseURL + "service/authors/" + str(author.id)
