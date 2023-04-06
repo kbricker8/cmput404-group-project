062c1bf88d4e2ee1f3f21bdfeef76c77697c33c2
jacob-gerun <jacob.gerun@gmail.com>
2023-04-06 17:47:59 -0600

    change base url

--- cmput404-group-project/vite.config.ts
+++ cmput404-group-project/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/kbricker8/cmput404-group-project/",
+  base: "/cmput404-group-project/",
   plugins: [react()],
 })
