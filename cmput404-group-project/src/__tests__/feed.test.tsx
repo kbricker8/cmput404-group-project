c05e414ba7fa75497512de320a996dd1fc844c1e
sxhuang9385 <sxhuang@ualberta.ca>
2023-04-07 16:50:54 -0600

    some tests for feed

--- cmput404-group-project/src/__tests__/feed.test.tsx
+++ cmput404-group-project/src/__tests__/feed.test.tsx
@@ -7,28 +7,78 @@ import MockAdapter from 'axios-mock-adapter';
 import Profile from '../routes/profile';
 import Album from '../routes/feed';
 import { OUR_API_URL, TEAM18_API_URL, TEAM7_API_URL } from '../consts/api_connections';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 
 const mock = new MockAdapter(axios);
 
 const mockData = {
-    // Mock the data returned from API calls 
-    posts: [],
+    posts: [
+        {
+          "id": "1",
+          type: "post",
+          title: "1",
+          source: "http://" + OUR_API_URL,
+          origin: "http://" + OUR_API_URL,
+          description: "",
+          contentType: "text/plain",
+          author : {},
+          categories: {},
+          count: 0,
+          content: "1",
+          comments: null,
+          published: "2023-03-13T04:52:57.692000Z",
+          visibility: "PUBLIC",
+          unlisted: false
+        },
+    ],
 };
 
 const testUserID = 'testUserId';
 
 describe('Feed post test', () => {
-    test('should fetch posts feed with page_size=999', async () => {
-      const mockedResponse = { data: 'mockedData' };
-      jest.spyOn(axios, 'get').mockResolvedValue(mockedResponse);
+  beforeEach(() => {
+    // Reset the mock before each test
+    mock.reset();
 
-      localStorage.setItem('user', JSON.stringify({ id: testUserID }));
-      localStorage.setItem('token', JSON.stringify('testToken'));
-      localStorage.setItem('USER_ID', testUserID);
-  
-      const response = await axios.get(
-        `${OUR_API_URL}service/authors/${testUserID}/posts/feed/?page_size=999`
-      );
-      expect(response).toEqual(mockedResponse);
-    });
+    localStorage.setItem('user', JSON.stringify({ id: testUserID }));
+    localStorage.setItem('token', JSON.stringify('testToken'));
+    localStorage.setItem('USER_ID', testUserID);
   });
+
+  test('should fetch posts feed with page_size=999', async () => {
+    const mockedResponse = { data: mockData.posts };
+    jest.spyOn(axios, 'get').mockResolvedValue(mockedResponse);
+
+    const logSpy = jest.spyOn(global.console, 'log');
+    const response = await axios.get(`${OUR_API_URL}service/authors/${testUserID}/posts/feed/?page_size=999`);
+    let consoleSpy = jest.spyOn(console, 'log');
+
+    console.log('This is a log message', response);
+    expect(consoleSpy).toHaveBeenCalledWith('This is a log message', response);
+
+    let consoleSpy2 = jest.spyOn(console, 'log');
+
+    console.log('This is a log message', response);
+    expect(consoleSpy2).toHaveBeenCalledWith('This is a log message', mockedResponse);
+
+    expect(response).toEqual(mockedResponse);
+  });
+
+  test('renders Album component', async () => {
+    const history = createMemoryHistory();
+        //const { getByLabelText,} = render(<NewPost />, { wrapper: MemoryRouter });
+        render(
+            <Router location={history.location} navigator={history}>
+                <Album />
+            </Router>
+        );
+    
+    const checkFeed = (expect, waitFor) => {
+      return expect.includes('View public posts here or publish your own!');
+    };
+    
+    await waitFor(() => {expect(screen.getByText(checkFeed)).toBeInTheDocument();});
+    expect(screen.getByText(checkFeed)).toBeInTheDocument();});
+  
+});
